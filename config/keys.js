e928cf8807fbab71fc3931851af65d72aa4fa16d
Christopher Cuna <chrohn@outlook.com>
2019-03-22 17:12:30 +0800

    Added git ignore data

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    ENV: process.env.NODE_ENV || "development",
-    PORT: process.env.PORT || 3000,
-    URL: process.env.URL || 'http://localhost:3000',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://mVf4gv:yuC0M1yUIoSvRn75@cluster0-vaciu.mongodb.net/mvfagbdb?retryWrites=true'
-}
